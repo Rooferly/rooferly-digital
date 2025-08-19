b7351ec2c9b2fae0ecf7405bf53ce9b1195d3a9e
Phil Diederich <pdiederich@waukeganroofing.com>
2025-08-19 12:37:05 -0500

    Fix caching issue - allow metadata updates to propagate

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
+            value: 'public, max-age=0, must-revalidate',
           },
         ],
       },
